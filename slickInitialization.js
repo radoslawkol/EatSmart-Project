cdba40f4008cd7cfa207b2de0f04cc81490c7dc6
Radosław Kołoczek <radoslaw.koloczek@gmail.com>
2021-10-11 23:09:27 +0200

    add dish page template - mobile version

---
+++ slickInitialization.js
@@ -0,0 +1,36 @@
+// SLICK INITIALIZATION
+
+$('.slick').slick({
+  lazyLoad: 'ondemand',
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
