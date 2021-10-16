c796a7e8c257cd57cbc571402fb27942925ad268
Radosław Kołoczek <radoslaw.koloczek@gmail.com>
2021-10-16 15:40:58 +0200

    add recipies page and create public folder

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+let initialPath = path.join(__dirname, '');
+
+app.use(express.static(initialPath));
+
 const mainPages = require('./routes/mainPages');
 const categories = require('./routes/categories');
+const exp = require('constants');
 
-app.use('/', mainPages);
-app.use('/categories', categories);
+// app.use('/', mainPages);
+// app.use('/categories', categories);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join());
+});
 
 app.listen(4000, () => {
   console.log('Server works');
