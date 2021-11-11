d3042065301902672b832553981e24fb330460cc
Radosław Kołoczek <radoslaw.koloczek@gmail.com>
2021-11-11 18:37:53 +0100

    Add articles rendering from api - Home page

--- public/js/homePage.js
+++ public/js/homePage.js
@@ -6,38 +6,44 @@ const headerHomePageSlick = document.querySelector('.headerHomePage__slick');
 
 const mediaQuery = window.matchMedia('(min-width: 992px)');
 
-const changeContent = function (size) {
-  if (size.matches) {
-    const html = `
-    <h2 class="mainHomePage__title">Artykuły</h2>
-        <ul class="mainHomePage__list">
-          <li class="mainHomePage__item">
-            <a href="" class="mainHomePage__link">Nazwa artykułu</a>
-          </li>
-          <li class="mainHomePage__item">
-            <a href="" class="mainHomePage__link">Nazwa artykułu</a>
-          </li>
-          <li class="mainHomePage__item">
-            <a href="" class="mainHomePage__link">Nazwa artykułu</a>
-          </li>
-          <li class="mainHomePage__item">
-            <a href="" class="mainHomePage__link">Nazwa artykułu</a>
-          </li>
-          <li class="mainHomePage__item">
-            <a href="" class="mainHomePage__link">Nazwa artykułu</a>
-          </li>
-          <li class="mainHomePage__item">
-            <a href="" class="mainHomePage__link">Nazwa artykułu</a>
-          </li>
-          <li class="mainHomePage__item">
-            <a href="" class="mainHomePage__link">Nazwa artykułu</a>
-          </li>
-        </ul>
-        <img src="/public/img/homePage-cook.svg" alt="" class="mainHomePage__illustration" />
-    `;
-    mainHomePageArticles.innerHTML = html;
-  } else {
-    return;
+const fetchArticles = async () => {
+  try {
+    const res = await fetch(`http://127.0.0.1:4000/api/v1/articles?sort=-date`);
+    const { data } = await res.json();
+    console.log(data);
+
+    let articlesElementsHtml = '';
+
+    data.articles.forEach((article) => {
+      const articleHtml = ` <li class="mainHomePage__item">
+              <a href="/articles/${article._id}" class="mainHomePage__link">${article.title}</a>
+            </li>`;
+      articlesElementsHtml += articleHtml;
+    });
+
+    return articlesElementsHtml;
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+const changeContent = async function (size) {
+  try {
+    if (size.matches) {
+      const elements = await fetchArticles();
+      const html = `
+        <h2 class="mainHomePage__title">Artykuły</h2>
+            <ul class="mainHomePage__list">
+              ${elements}
+            </ul>
+            <img src="/public/img/homePage-cook.svg" alt="" class="mainHomePage__illustration" />
+        `;
+      mainHomePageArticles.innerHTML = html;
+    } else {
+      return;
+    }
+  } catch (err) {
+    console.log(err.message);
   }
 };
 
