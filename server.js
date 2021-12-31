a54cdca48d9e9543aac0404f3946c4ee60128823
Radosław Kołoczek <radoslaw.koloczek@gmail.com>
2021-12-31 14:30:24 +0100

    Fix uncaught exception middleware

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
-// process.on('uncaughtException', (err) => {
-//   console.log('UNCAUGHT EXCEPTION!  Shutting down...');
-//   console.log(err.name, err.message);
-//   server.close(() => {
-//     process.exit(1);
-//   });
-// });
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION!  Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 
 dotenv.config({ path: './config.env' });
 
